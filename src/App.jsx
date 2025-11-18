74d1e84cd3ffb740b11dd8d95ec00bbcc7ef0ea7
Lozano Mora Diego <lozano.mora.diego@gmail.com>
2025-11-18 13:34:47 -0500

    Fix: correcion ubicacion de archivos

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Headers from './componentes/componente_General/Header'
+import Footer from './componentes/componente_General/Footer'
+import Login from './componentes/componente_General/Login'
+import Info from './componentes/componente_General/Info'
+import Confi from './componentes/componente_Perfil/Confi'
+import Noticies from './componentes/componente_Foro/Noticie'
+import VerPerfil from './componentes/componente_Perfil/VerPerfil'
+import Ranking from './componentes/componente_General/Ranking'
+import Home from './pages/Home'
+import Perfil from './pages/Perfil'
+import Biblioteca from './pages/Biblioteca'
+import Foro from './pages/Foro'
+
+function App() {
+  return (
+    <>
+      <Headers />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/perfil" element={<Perfil />} />
+        <Route path="/perfil/:id" element={<VerPerfil />} />
+        <Route path="/Confi" element={<Confi />} />
+        <Route path="/info/:id" element={<Info />} />
+        <Route path="/foro" element={<Foro />} />
+        <Route path="/Noticies" element={<Noticies />} />
+        <Route path="/Ranking" element={<Ranking />} />
+        <Route path="/biblioteca" element={<Biblioteca />} />
+        <Route path="/Login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </>
+  )
+}
+export default App
